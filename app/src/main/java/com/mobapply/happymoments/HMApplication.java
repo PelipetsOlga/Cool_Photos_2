9ad02c96cae2e1eb0798e64b018189339b9019fe
apelipets <a.pelipets@videal.net>
2015-08-10 13:14:56 +0300

    Initial commit

---
+++ app/src/main/java/com/mobapply/happymoments/HMApplication.java
@@ -0,0 +1,14 @@
+package com.mobapply.happymoments;
+
+import android.app.Application;
+
+/**
+ * Created by apelipets on 8/10/15.
+ */
+public class HMApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
