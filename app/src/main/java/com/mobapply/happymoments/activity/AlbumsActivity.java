74672b107fdb8b01e0b6c0edf0da40feee768d8b
apelipets <a.pelipets@videal.net>
2015-08-10 18:36:29 +0300

    added empty content provider

--- app/src/main/java/com/mobapply/happymoments/activity/AlbumsActivity.java
+++ app/src/main/java/com/mobapply/happymoments/activity/AlbumsActivity.java
@@ -48,19 +48,6 @@ public class AlbumsActivity extends ActionBarActivity
     public void onNavigationDrawerItemSelected(int position) {
     }
 
-    public void onSectionAttached(int number) {
-        switch (number) {
-            case 1:
-                mTitle = getString(R.string.title_section1);
-                break;
-            case 2:
-                mTitle = getString(R.string.title_section2);
-                break;
-            case 3:
-                mTitle = getString(R.string.title_section3);
-                break;
-        }
-    }
 
     public void restoreActionBar() {
         ActionBar actionBar = getSupportActionBar();
@@ -68,8 +55,6 @@ public class AlbumsActivity extends ActionBarActivity
         actionBar.setDisplayShowTitleEnabled(true);
         actionBar.setTitle(mTitle);
     }
-
-
 
 
 
---
+++ app/src/main/java/com/mobapply/happymoments/utils/AppUtils.java
@@ -0,0 +1,7 @@
+package com.mobapply.happymoments.utils;
+
+/**
+ * Created by apelipets on 8/10/15.
+ */
+public class AppUtils {
+}
