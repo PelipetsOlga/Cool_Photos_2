5b6a5635b7b1b94e9e4fed4f9c1fc5c01f546812
pelipetsaa <pelipetsaa@gmail.com>
2015-08-11 21:03:47 +0300

    Settings layout

--- app/src/main/java/com/mobapply/happymoments/activity/AlbumsActivity.java
+++ app/src/main/java/com/mobapply/happymoments/activity/AlbumsActivity.java
@@ -1,13 +1,19 @@
 package com.mobapply.happymoments.activity;
 
 import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.GridView;
 
 import com.mobapply.happymoments.R;
+import com.mobapply.happymoments.dialog.NewAlbumDialog;
 import com.mobapply.happymoments.menu.NavigationDrawerFragment;
 import com.mobapply.happymoments.utils.HappyMomentsUtils;
 
@@ -38,9 +44,17 @@ public class AlbumsActivity extends ActionBarActivity
                 R.id.navigation_drawer,
                 (DrawerLayout) findViewById(R.id.drawer_layout));
 
-        //albums container
-        GridView grid= (GridView) findViewById(R.id.gridView);
+        FloatingActionButton fab= (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NewAlbumDialog dialog=new NewAlbumDialog();
+                dialog.show(getFragmentManager(),null);
+            }
+        });
 
+        //albums container
+        GridView grid = (GridView) findViewById(R.id.gridView);
 
 
     }
@@ -50,7 +64,6 @@ public class AlbumsActivity extends ActionBarActivity
     }
 
 
-
     public void restoreActionBar() {
         ActionBar actionBar = getSupportActionBar();
         actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
@@ -58,5 +71,22 @@ public class AlbumsActivity extends ActionBarActivity
         actionBar.setTitle(mTitle);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_select_albums:
+                //TODO
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 }
--- app/src/main/java/com/mobapply/happymoments/dialog/NewAlbumDialog.java
+++ app/src/main/java/com/mobapply/happymoments/dialog/NewAlbumDialog.java
@@ -1,14 +1,55 @@
 package com.mobapply.happymoments.dialog;
 
+import android.app.AlertDialog;
 import android.app.Dialog;
+import android.app.DialogFragment;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.mobapply.happymoments.R;
 
 /**
  * Created by apelipets on 8/10/15.
  */
-public class NewAlbumDialog extends Dialog{
+public class NewAlbumDialog extends DialogFragment {
 
-    public NewAlbumDialog(Context context) {
-        super(context);
+    public NewAlbumDialog() {
     }
+
+    @Override
+    public Dialog onCreateDialog(Bundle bundle) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        EditText view = new EditText(getActivity());
+        view.setHint(getActivity().getResources().getString(R.string.dialog_hint));
+        view.setPadding(16,24,16,24);
+        builder.setTitle(getActivity().getResources().getString(R.string.dialog_title));
+        builder.setPositiveButton(getActivity().getResources().getString(R.string.btn_save), new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+
+        builder.setNegativeButton(getActivity().getResources().getString(R.string.btn_cancel), new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+
+        builder.setView(view);
+        builder.setCancelable(true);
+
+        return builder.create();
+    }
+
 }
