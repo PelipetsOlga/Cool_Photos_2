fcf871aa9efe21fe8c25ee89aa6f304290133a0e
pelipetsaa <pelipetsaa@gmail.com>
2015-08-13 16:24:40 +0300

    Settings layout

---
+++ app/src/main/java/com/mobapply/happymoments/adapter/AlbumImageView.java
@@ -0,0 +1,37 @@
+package com.mobapply.happymoments.adapter;
+
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+public class AlbumImageView  extends ImageView {
+
+    public AlbumImageView(Context context) {
+        super(context);
+    }
+
+    public AlbumImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public AlbumImageView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+
+    @Override
+    protected void onMeasure(final int widthMeasureSpec, final int heightMeasureSpec) {
+        final int width = getDefaultSize(getSuggestedMinimumWidth(),widthMeasureSpec);
+        final int height=width*4/3;
+        setMeasuredDimension(width, height);
+    }
+
+    @Override
+    protected void onSizeChanged(final int w, final int h, final int oldw, final int oldh) {
+        super.onSizeChanged(w, w, oldw, oldh);
+    }
+
+
+}
