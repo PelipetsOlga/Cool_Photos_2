2516b460186bd169705ca6f0bb70ef899c22d5ac
Olga Pelipets <pelipets.olga.developer@gmail.com>
2017-03-18 21:16:58 +0200

    ru

--- app/build/generated/source/r/debug/com/melnykov/fab/R.java
+++ app/build/generated/source/r/debug/com/melnykov/fab/R.java
@@ -34,16 +34,16 @@ public final class R {
 		public static final int normal = 0x7f0c000d;
 	}
 	public static final class string {
-		public static final int define_FloatingActionButton = 0x7f06004c;
-		public static final int library_FloatingActionButton_author = 0x7f060058;
-		public static final int library_FloatingActionButton_authorWebsite = 0x7f060059;
-		public static final int library_FloatingActionButton_isOpenSource = 0x7f06005a;
-		public static final int library_FloatingActionButton_libraryDescription = 0x7f06005b;
-		public static final int library_FloatingActionButton_libraryName = 0x7f06005c;
-		public static final int library_FloatingActionButton_libraryVersion = 0x7f06005d;
-		public static final int library_FloatingActionButton_libraryWebsite = 0x7f06005e;
-		public static final int library_FloatingActionButton_licenseId = 0x7f06005f;
-		public static final int library_FloatingActionButton_repositoryLink = 0x7f060060;
+		public static final int define_FloatingActionButton = 0x7f060061;
+		public static final int library_FloatingActionButton_author = 0x7f060067;
+		public static final int library_FloatingActionButton_authorWebsite = 0x7f060068;
+		public static final int library_FloatingActionButton_isOpenSource = 0x7f060069;
+		public static final int library_FloatingActionButton_libraryDescription = 0x7f06006a;
+		public static final int library_FloatingActionButton_libraryName = 0x7f06006b;
+		public static final int library_FloatingActionButton_libraryVersion = 0x7f06006c;
+		public static final int library_FloatingActionButton_libraryWebsite = 0x7f06006d;
+		public static final int library_FloatingActionButton_licenseId = 0x7f06006e;
+		public static final int library_FloatingActionButton_repositoryLink = 0x7f06006f;
 	}
 	public static final class styleable {
 		public static final int[] FloatingActionButton = { 0x010100d4, 0x7f01001a, 0x7f010057, 0x7f010058, 0x7f010059, 0x7f01005a, 0x7f01005b, 0x7f01005c, 0x7f01005d, 0x7f01005e, 0x7f01005f, 0x7f010060, 0x7f010061, 0x7f010062, 0x7f010063, 0x7f010064, 0x7f010065, 0x7f010066, 0x7f010067, 0x7f010068, 0x7f010069, 0x7f01006a, 0x7f01006b, 0x7f01006c, 0x7f01006d, 0x7f01006e, 0x7f01006f, 0x7f010070, 0x7f010159, 0x7f01015a };
